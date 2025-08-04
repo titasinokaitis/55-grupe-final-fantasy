3651a76708ff9e12a6697b9b130f5f3573cd39ea
titasinokaitis <titas.inokaitis@gmail.com>
2025-08-04 10:20:11 +0300

    home

--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router';
+import heroImg from '../../assets/hero.png';
+
 export function HomePage() {
     return (
-        <h1>HOME PAGE CONTENT</h1>
+        <main>
+            <div className="container col-xxl-8 px-4 py-5">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                    <div className="col-10 col-sm-8 col-lg-6">
+                        <img src={heroImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes"
+                            width={700} height={500} loading="lazy" />
+                    </div>
+                    <div className="col-lg-6">
+                        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Final fantasy movies listing</h1>
+                        <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                            <Link href="/movies" className="btn btn-primary btn-lg px-4 me-md-2">All movies</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
