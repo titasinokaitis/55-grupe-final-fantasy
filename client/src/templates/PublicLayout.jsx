87a92c40f2265353cf21ce1a0174c8d5105d9e11
titasinokaitis <titas.inokaitis@gmail.com>
2025-08-04 09:45:43 +0300

    header

--- client/src/templates/PublicLayout.jsx
+++ client/src/templates/PublicLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import { Header } from "../components/Header";
 
 export function PublicLayout() {
     return (
         <>
-            <header>HEADER CONTENT</header>
+            <Header />
             <Outlet />
             <footer>FOOTER CONTENT</footer>
         </>
